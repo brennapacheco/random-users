d8bb78b5e00e94bf9ea6588f3665796c12355e28
Brenna Pacheco <brennapacheco@hotmail.com>
2024-04-02 11:28:29 +0100

    Initial Commit

---
+++ RandomUsers/RandomUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUsersApp.swift
+//  RandomUsers
+//
+//  Created by Brenna Pacheco  on 02/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct RandomUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
