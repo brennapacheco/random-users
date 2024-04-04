d2fa87e734b7fb997835ebd8504b63649b59e956
Brenna Pacheco <brennapacheco@hotmail.com>
2024-04-04 15:57:27 +0100

    Created User model and Name model

---
+++ User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  RandomUsers
+//
+//  Created by Brenna Pacheco  on 04/04/2024.
+//
+
+import Foundation
+
+struct User: Identifiable {
+    let id: String
+    let name: Name
+}
+
+struct Name {
+    let title: String
+    let first: String
+    let last: String
+}
